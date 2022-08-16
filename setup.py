7312a912fab1c07bab997d662a89c754327007b3
haeussma <83341109+haeussma@users.noreply.github.com>
2022-08-16 15:45:10 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+from setuptools import setup
+
+setup(
+    name="calibration_data",
+    version="0.0.1",
+    description="Datamodel, handling calibration data from UV-Vis and HPLC measurements",
+    url="https://github.com/FAIRChemistry/calibration_data",
+    author="Haeussler, Max",
+    author_email="st171427@stud.uni-stuttgart.de",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+)

292a156b61da0f16dc780a8cd6d17d5ba148ef7d
haeussma <83341109+haeussma@users.noreply.github.com>
2022-10-21 17:13:53 +0200

    added classmethod for SpectraMax190

--- datamodel_calibration/core/calibration.py
+++ datamodel_calibration/core/calibration.py
@@ -1,7 +1,9 @@
 import sdRDM
+import numpy as np
+import pandas as pd
 
 from typing import Optional
-from typing import Optional, Union
+from typing import Optional, Union, Dict
 from pydantic import PrivateAttr
 from pydantic import Field
 from sdRDM.base.listplus import ListPlus
@@ -11,6 +13,7 @@ from sdRDM.base.utils import forge_signature, IDGenerator
 from .device import Device
 from .standardcurve import StandardCurve
 from .spectrum import Spectrum
+from .series import Series
 
 
 class Calibration(sdRDM.DataModel):
@@ -36,6 +39,48 @@ class Calibration(sdRDM.DataModel):
     spectrum: Optional[Spectrum] = Field(
         description="UVVisSpectrum object, containing spectrum data", default=None
     )
+
+    @classmethod
+    def SpectraMax190_StandardCurve(
+        cls, 
+        path: str, 
+        species_wavelengths: Dict, 
+        replicates: int,
+        ):
+
+        df = pd.read_excel(path, skiprows=17)
+        temperature = df["Temperature(¡C)"].loc[0]
+        df = df.drop(columns=["Unnamed: 0", "Temperature(¡C)"])
+        df = df.dropna(how="all", axis=0)
+        df = df.loc[:replicates]
+        if isinstance(wavelengths, list) and len(wavelengths) > 1:
+            n_concentrations = int(df.shape[1]/len(wavelengths))
+            for wavelength in range(len(wavelengths)):
+                if wavelength != 0:
+                    df = df.drop(df.columns[n_concentrations*wavelength], axis=1)
+            values = df.values.reshape(replicates, len(wavelengths), n_concentrations)
+
+        else: 
+            n_concentrations = df.shape[1]
+            values = df.values.reshape(replicates, n_concentrations)
+
+        absorptions = []
+        for replicate in values:
+            absorptions.append(Series(values=list(replicate)))
+
+        instance = cls(
+            device = Device(
+                device_manufacturer="Molecular Devices",
+                device_model = "Spectra Max 190 Microplate Reader"
+            ),
+            standard_curve = StandardCurve(
+                temperature=temperature,
+                temperature_unit="C",
+                absorption = absorptions
+            )
+        )
+
+        return instance
 
     __repo__: Optional[str] = PrivateAttr(
         default="git://github.com/FAIRChemistry/datamodel_calibration.git"

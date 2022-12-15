62b6c24783ee23ceaae2f8dbbdfaba4a1176d25b
haeussma <83341109+haeussma@users.noreply.github.com>
2022-12-15 18:02:06 +0100

    adjusted requirements

--- setup.py
+++ setup.py
@@ -3,19 +3,18 @@ from setuptools import setup
 
 setup(
     name="CaliPytion",
-    version="0.0.2",
+    version="0.0.3",
     description="Data model and standard curve functionalities for calibration data",
     url="https://github.com/FAIRChemistry/calibration_data",
     author="Haeussler, Max",
     author_email="st171427@stud.uni-stuttgart.de",
     packages=setuptools.find_packages(),
     install_requires=[
-        "ipython",
-        "lmfit==1.0.3",
-        "matplotlib==3.6.2",
-        "numpy==1.22.3",
-        "pandas==1.4.2",
-        "pydantic==1.9.0",
-        "scipy==1.8.1",
-        "setuptools==61.2.0"]
+        "lmfit",
+        "matplotlib",
+        "numpy",
+        "pandas",
+        "pydantic==1.8.2",
+        "scipy",
+        "setuptools"]
     )

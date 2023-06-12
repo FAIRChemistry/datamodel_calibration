f1ecb2ab04e2c5d036eefb8bae161888c31a1e9e
haeussma <83341109+haeussma@users.noreply.github.com>
2023-06-12 09:52:28 +0200

    cleaning

--- CaliPytion/tools/equations.py
+++ CaliPytion/tools/equations.py
@@ -1,6 +1,6 @@
 from sympy import Symbol, exp, Equality
 
-# define symbols 
+# define symbols
 a = Symbol("a")
 b = Symbol("b")
 c = Symbol("c")
@@ -8,8 +8,10 @@ concentration = Symbol("concentration")
 signal = Symbol("signal")
 
 # define calibration equations
-linear = Equality((a*concentration), signal)
-quadratic = Equality((a*concentration**2 + b*concentration), signal)
-poly_3 = Equality((a*concentration**3 + b*concentration**2 + c*concentration), signal)
-poly_e = Equality((a*exp(concentration/b)), signal)
-rational = Equality(((a*concentration) / (b+concentration)), signal)
+linear = Equality((a * concentration), signal)
+quadratic = Equality((a * concentration**2 + b * concentration), signal)
+poly_3 = Equality(
+    (a * concentration**3 + b * concentration**2 + c * concentration), signal
+)
+poly_e = Equality((a * exp(concentration / b)), signal)
+rational = Equality(((a * concentration) / (b + concentration)), signal)

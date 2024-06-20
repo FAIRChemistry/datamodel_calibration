1b220f1361adc0c0fa6205aaee978fdba35fca01
Max Häußler <max.haeussler@gmail.com>
2024-06-20 23:22:07 +0200

    Delete CaliPytion directory

--- CaliPytion/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from .core.calibrationmodel import CalibrationModel
-from .core.calibrationrange import CalibrationRange
-from .core.fitstatistics import FitStatistics
-from .core.parameter import Parameter
-from .core.sample import Sample
-from .core.signaltype import SignalType
-from .core.standard import Standard
--- CaliPytion/core/signaltype.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class SignalType(Enum):
-
-    ABSORBANCE = "absorbance"
-    TRANSMITTANCE = "transmittance"
-    REFLECTANCE = "reflectance"
